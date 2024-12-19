8f8da7a86518f6e7b2ed1ad7c21889f17c5775f3
Uday0458 <166024876+Uday0458@users.noreply.github.com>
2024-12-19 12:57:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var http = require('http');
 
 //create a server object:
 http.createServer(function (req, res) {
-  res.write('Welcome to the world of coding....This is Udaykumar....!!... '); //write a response to the client
+  res.write('Welcome to the world of coding....This is Udaykumar Nadivinti....!!... '); //write a response to the client
   res.end(); //end the response
 }).listen(4000); //the server object listens on port 4000
